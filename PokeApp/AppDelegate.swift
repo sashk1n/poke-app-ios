5db33c93ecb4e23b2a01e00b274b520207ae9ac2
Alexander Marcenyuk <alexander.marcenyuk@gmail.com>
2019-07-07 08:03:25 +0300

    initial commit

---
+++ PokeApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  PokeApp
+//
+//  Created by marcenuk on 06/07/2019.
+//  Copyright © 2019 marcenyuk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = UIViewController()
+        homeViewController.view.backgroundColor = UIColor.red
+        window!.rootViewController = homeViewController
+        window!.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ PokeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeApp
+//
+//  Created by marcenuk on 06/07/2019.
+//  Copyright © 2019 marcenyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
