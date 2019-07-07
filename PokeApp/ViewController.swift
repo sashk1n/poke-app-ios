719a411395a1366bd8773d039406b9d4884873a3
Alexander Marcenyuk <alexander.marcenyuk@gmail.com>
2019-07-07 08:07:52 +0300

    remove storyboard

--- PokeApp/ViewController.swift
+++ PokeApp/ViewController.swift
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
